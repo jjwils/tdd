1262495abbfb073589b4e80febc884e8aa25f127
jjwils <mail@johnwilson.me.uk>
2022-07-21 17:26:01 +0100

    ben

--- src/test/java/uk/me/johnwilson/SetTest.java
+++ src/test/java/uk/me/johnwilson/SetTest.java
@@ -8,4 +8,37 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class SetTest {
+
+    BenJohnSet benJohnSet;
+
+
+    @Test
+    public void isEmpty(){
+        benJohnSet = new BenJohnSet();
+        assertTrue(benJohnSet.isEmpty());
+    }
+
+
+    @Test
+    public void isNotEmpty(){
+
+        benJohnSet = new BenJohnSet();
+        benJohnSet.add();
+        assertFalse(benJohnSet.isEmpty());
+
+    }
+
+    @Test
+    public void size(){
+        benJohnSet = new BenJohnSet();
+        assertTrue(benJohnSet.size() == 0);
+
+        benJohnSet.add();
+        assertTrue(benJohnSet.size() == 1);
+
+
+    }
+
+
+
 }
